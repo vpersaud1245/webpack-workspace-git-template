89d2ebcf1a72e65691806d11fada35b14fbee7d1
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-01-13 16:36:03 -0500

    Add and configure dependencies

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+};
